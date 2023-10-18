c7dd212322a8e155f42f8045097a9034b94f335c
Tanguy <tanguy.marsault@desy.de>
2023-10-18 15:03:22 +0200

    config

---
+++ config.py
@@ -0,0 +1,12 @@
+### SIMULATION VARIABLES ###
+
+N  = 100 #number of particle
+T  = 100 #in Kelvin, temperature for the simulation
+Lx = 1 #in cm dimension in direction x
+Ly = 1 #direction y
+Lz = 1 #direction z
+
+### Physics Constants ###
+
+hbar = 6.02*10**{-34} ## J.s
+kb   = 1.38*10**{-23} # J.K^-1
---
+++ main.py
@@ -0,0 +1,9 @@
+from config import *
+
+print(N)
+
+def init(): #initialize parameters
+    Ex=
+    Ey=
+    Ez=
+    n_cut= #max number of states in a direction
