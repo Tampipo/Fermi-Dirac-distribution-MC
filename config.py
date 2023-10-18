192e0e1255510c4a62ef262eefb3e78fda70a579
Tanguy <tanguy.marsault@desy.de>
2023-10-18 16:00:55 +0200

    initialization

--- config.py
+++ config.py
@@ -1,12 +1,15 @@
+import numpy as np
+
 ### SIMULATION VARIABLES ###
 
 N  = 100 #number of particle
-T  = 100 #in Kelvin, temperature for the simulation
-Lx = 1 #in cm dimension in direction x
-Ly = 1 #direction y
-Lz = 1 #direction z
+T  = 10 #in Kelvin, temperature for the simulation
+Lx = 10**(-6) #in m dimension in direction x
+Ly = 10**(-6) #direction y
+Lz = 10**(-6) #direction z
 
 ### Physics Constants ###
 
-hbar = 6.02*10**{-34} ## J.s
-kb   = 1.38*10**{-23} # J.K^-1
+hbar = 6.02*10**(-34) ## J.s
+kb   = 1.38*10**(-23) # J.K^-1
+me   = 9.11*10**(-31) #kg
--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 from config import *
-
+import numpy as np
 print(N)
 
 def init(): #initialize parameters
-    Ex=
-    Ey=
-    Ez=
-    n_cut= #max number of states in a direction
+    Ex=(hbar*2*np.pi)**2/(2*me*Lx**2*kb*T) #dimensionless energy in x direction
+    Ey=(hbar*2*np.pi)**2/(2*me*Ly**2*kb*T) #y direction
+    Ez=(hbar*2*np.pi)**2/(2*me*Lz**2*kb*T) #z direction
+    n_cut=0 #max number of states in a direction
+    config_dict={} #initialize particle states
+    n_x=0
+    n_y=0
+    n_z=0
+    s=1
+    for i in range (1,N):
+        if n_x>n_y:
+            n_y+=1
+        elif n_y>n_z:
+            n_z+=1
+        elif s==1:
+            s=-1
+        else :
+            n_x+=1
+        config_dict[f'i']=[n_x,n_y,n_z,s] #state is given by n_x,n_y,n_z,s
+    return [Ex,Ey,Ez,n_cut,config_dict]
+
+
+def main():
+    print(init())
+
+if __name__=="__main__":
+    print(36*hbar**2,me*kb)
+    main()
